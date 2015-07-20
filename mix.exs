71dcb68aab27fdadd946f793d7849e7d02638d08
David Ruyer <david.ruyer@jobteaser.com>
2015-07-20 12:41:39 +0200

    phoenix upgrade

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule Jtv.Mixfile do
   #
   # Type `mix help deps` for examples and options
   defp deps do
-    [{:phoenix, "~> 0.13.1"},
+    [{:phoenix, "~> 0.14.0"},
      {:phoenix_ecto, "~> 0.4"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 1.0"},
